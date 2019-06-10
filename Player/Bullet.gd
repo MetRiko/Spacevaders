ec8f2b60c93c8ffa1ac4eb7cfa9f49a31bcf47ce
Unknown <18maximus8050@gmail.com>
2019-06-11 00:40:52 +0200

    Changed version on more lightweight

--- Player/Bullet.gd
+++ Player/Bullet.gd
@@ -2,7 +2,6 @@ extends KinematicBody2D
 
 var vel = Vector2(0.0, -10.0)
 
-
 func _physics_process(delta):
 	delta *= 60.0
 	move(delta)
@@ -12,6 +11,7 @@ func _physics_process(delta):
 		
 func move(delta):
 	var result = move_and_collide(vel*delta)
+
 	if result and result.collider.is_in_group("Enemy"):
 		result.collider.damage(1)
 		queue_free()
